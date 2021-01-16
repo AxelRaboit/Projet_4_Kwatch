56217c3039cfef161d1bcb5859c6e03ccb0629b5
AxelRaboit <axel.raboit@gmail.com>
2021-01-16 13:08:36 +0100

    rebuilt and path changed for the login and register

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use LogicException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -31,6 +32,9 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_login');
+/*         throw new LogicException(
+            'This method can be blank - it will be intercepted by the logout key on your firewall.'
+        ); */
     }
 }
