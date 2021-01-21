d220a994b89ecf0b09b48be7331bdee39a1e002a
AxelRaboit <axel.raboit@gmail.com>
2021-01-21 01:32:16 +0100

    add crud for the episode, season, and program

---
+++ src/Form/ProgramType.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Program;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ProgramType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'required' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner le titre de la série.',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Écrire {{ limit }} caractères minimum.',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 50,
+                    ]),
+                ],
+            ])
+            ->add('synopsis', TextareaType::class, [
+                'label' => 'Synopsis',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner le synopsis de la série.',
+                    ]),
+                    new Length([
+                        'min' => 40,
+                        'minMessage' => 'Écrire {{ limit }} caractères minimum.',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4000,
+                    ]),
+                ],
+            ])
+            ->add('poster', TextType::class, [
+                'label' => 'Poster'
+            ])
+            ->add('country', TextType::class, [
+                'label' => 'Pays',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner le pays de la série.',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Écrire {{ limit }} caractères minimum.',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 50,
+                    ]),
+                ],
+            ])
+            ->add('category', null, ['choice_label' => 'name'])
+            ->add('save', SubmitType::class, [
+                'label' => 'Sauvegarder'
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Program::class,
+        ]);
+    }
+}
