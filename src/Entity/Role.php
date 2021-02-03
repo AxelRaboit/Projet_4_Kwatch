06f2e0a6c9296c5e30405c2bd79563083cd41b65
AxelRaboit <axel.raboit@gmail.com>
2021-02-03 14:48:14 +0100

    remove the relation ManyToMany between actor and program and replace it by a new entity role

---
+++ src/Entity/Role.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RoleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RoleRepository::class)
+ */
+class Role
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Actor::class, inversedBy="roles")
+     */
+    private $actor;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Program::class, inversedBy="role")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $program;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getActor(): ?Actor
+    {
+        return $this->actor;
+    }
+
+    public function setActor(?Actor $actor): self
+    {
+        $this->actor = $actor;
+
+        return $this;
+    }
+
+    public function getProgram(): ?Program
+    {
+        return $this->program;
+    }
+
+    public function setProgram(?Program $program): self
+    {
+        $this->program = $program;
+
+        return $this;
+    }
+}
