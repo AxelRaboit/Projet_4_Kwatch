cdec2053ff03df25f7f914f8c094621ce831c400
AxelRaboit <axel.raboit@gmail.com>
2021-02-03 17:20:18 +0100

    fix

--- src/Controller/Actor/ActorController.php
+++ src/Controller/Actor/ActorController.php
@@ -4,11 +4,12 @@ namespace App\Controller\Actor;
 
 use App\Entity\Actor;
 use App\Form\ActorType;
+use App\Repository\RoleRepository;
 use App\Repository\ActorRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/actor")
@@ -61,10 +62,13 @@ class ActorController extends AbstractController
     /**
      * @Route("/{id}", name="actor_show", methods={"GET"})
      */
-    public function show(Actor $actor): Response
+    public function show(Actor $actor, RoleRepository $roleRepository): Response
     {
+        $role = $roleRepository->findOneBy(['actor' => $actor->getId()]);
+
         return $this->render('actor/show.html.twig', [
             'actor' => $actor,
+            'role' => $role
         ]);
     }
 
