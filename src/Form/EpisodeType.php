295b03035a23a553ff380a79a62c6bec5f86f6f9
AxelRaboit <axel.raboit@gmail.com>
2021-02-06 17:39:45 +0100

    episodeType finished

--- src/Form/EpisodeType.php
+++ src/Form/EpisodeType.php
@@ -9,16 +9,26 @@ use Vich\UploaderBundle\Form\Type\VichFileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class EpisodeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('number')
-            ->add('summary')
+            ->add('title', TextType::class, [
+                'label' => 'Titre']
+            )
+            ->add('number', IntegerType::class, [
+                'label' => 'Numéro de l\'épisode'
+            ])
+            ->add('summary', TextareaType::class, [
+                'label' => 'Sommaire de l\'épisode'
+            ])
             ->add('imageFile', VichFileType::class, [
+                'label' => 'Image de l\'épisode',
                 'required'      => false,
                 'allow_delete'  => false, // not mandatory, default is true
                 'download_uri'  => false, // not mandatory, default is true
